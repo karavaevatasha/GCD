0f52986c2ab5f6528f9893b534e606bf3c2b3993
Наталия Караваева <karavaeva502@gmail.com>
2020-10-14 19:36:07 +0800

    Initial Commit

---
+++ AsynchronousPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AsynchronousPractice
+//
+//  Created by Nataliia on 14.10.2020.
+//  Copyright © 2020 Natalia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
