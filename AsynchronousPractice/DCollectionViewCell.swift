e5d8b7d11839a7fbf78503a65baaee67e3164a4e
Наталия Караваева <karavaeva502@gmail.com>
2020-10-27 21:13:08 +0800

    first commit

---
+++ AsynchronousPractice/DCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DCollectionViewCell.swift
+//  AsynchronousPractice
+//
+//  Created by Nataliia on 19.10.2020.
+//  Copyright © 2020 Natalia. All rights reserved.
+//
+
+import UIKit
+
+class DCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ AsynchronousPractice/getTheImage.swift
@@ -0,0 +1,9 @@
+//
+//  getTheImage.swift
+//  AsynchronousPractice
+//
+//  Created by Nataliia on 22.10.2020.
+//  Copyright © 2020 Natalia. All rights reserved.
+//
+
+import Foundation
